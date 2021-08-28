f12a8887356876a3c6170e1e23ec3dd6b9bdf282
Jagadish Mohanty <j.or.mohanty@gmail.com>
2021-08-28 12:28:05 +0530

    Final

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,6 +4,8 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
 import { UserSettingsFormComponent } from './user-settings-form/user-settings-form.component';
+import { HttpClientModule } from '@angular/common/http';
+import { ButtonsModule } from 'ngx-bootstrap/buttons'
 
 @NgModule({
   declarations: [
@@ -12,7 +14,9 @@ import { UserSettingsFormComponent } from './user-settings-form/user-settings-fo
   ],
   imports: [
     BrowserModule,
-    FormsModule
+    FormsModule,
+    HttpClientModule,
+    ButtonsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
--- src/app/user-settings-form/user-settings-form.component.ts
+++ src/app/user-settings-form/user-settings-form.component.ts
@@ -1,4 +1,8 @@
 import { Component, OnInit } from '@angular/core';
+import { NgForm, NgModel } from '@angular/forms';
+import { Observable } from 'rxjs';
+import { DataService } from '../data/data.service';
+import { UserSettings } from '../data/user-settings';
 
 @Component({
   selector: 'app-user-settings-form',
@@ -7,9 +11,53 @@ import { Component, OnInit } from '@angular/core';
 })
 export class UserSettingsFormComponent implements OnInit {
 
-  constructor() { }
+  singleModel = "On";
+  originalUserSettings: UserSettings = {
+    name: 'Milton',
+    emailOffers: true,
+    interfaceStyle: 'dark',
+    subscriptionType: 'Annual',
+    notes: 'here are some notes...'
+  };
+
+
+  userSettings: UserSettings = { ...this.originalUserSettings }
+  postError = false;
+  postErrorMessage = '';
+  subscriptionTypes!: Observable<string[]>;
+
+  constructor(private dataService: DataService) { }
 
   ngOnInit(): void {
+    this.subscriptionTypes = this.dataService.getSubscriptionTypes();
+  }
+
+  onHttpError(errorResponse: any) {
+    console.log('error: ', errorResponse);
+    this.postError = true;
+    console.log('IspostError: ', this.postError);
+    this.postErrorMessage = errorResponse.error.errorMessage;
+    console.log('ErrorMessage: ', this.postErrorMessage);
+  }
+
+  onSubmit(form: NgForm) {
+    console.log('in onSubmit: ', form.valid);
+
+    if (form.valid) {
+      this.dataService.postUserSettingsForm(this.userSettings)
+        .subscribe(
+          result => console.log('success: ', result),
+          error => this.onHttpError(error)
+        );
+    }
+    else {
+      this.postError = true;
+      this.postErrorMessage = "Please fix the above errors";
+    }
+  }
+
+  onBlur(field: NgModel) {
+    console.log('in onBlur: ', field.valid);
   }
 
 }
